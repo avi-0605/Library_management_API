1066e9a2b1f3a402052867e60e432ebae8a32d19
avi-0605 <aavanirp@gmail.com>
2025-12-01 11:24:53 +0530

    added uri

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/library-management-api');
+mongoose.connect(process.env.MongoUri);
 
 const db = mongoose.connection;
 
